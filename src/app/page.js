51d9054c37f9fc5ee0e07baaa8f325b0c2357cd4
Malik-444 <talhahusnain475@gmail.com>
2024-02-03 21:52:15 +0500

    middleware add

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
+"use client"
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const AlertBtn=()=>{
+    alert("are you confirm")
+  }
   return (
     <div className="flex flex-col gap-y-3">
-      <h1 className="text-6xl flex justify-center items-center mt-32 ">Hello Auth</h1>
+      <h1 className="text-6xl flex justify-center items-center mt-32 ">
+        Hello Auth
+      </h1>
+      <Link
+        href={"http://localhost:3000/api/auth/signin/github"}
+        onClick={AlertBtn}
+      >
+        {" "}
+        SIgnIn to github
+      </Link>
     </div>
   );
 }
