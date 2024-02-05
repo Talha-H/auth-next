757d480d9d0ab4ed6425af694720a098e51d995f
Malik-444 <talhahusnain475@gmail.com>
2024-02-05 22:09:20 +0500

    LogIn Extra

--- src/app/extra/page.jsx
+++ src/app/extra/page.jsx
@@ -1,17 +1,21 @@
 "use client";
 import React from "react";
 import { useSession } from "next-auth/react";
-import { redirect } from "next/dist/server/api-utils";
 import LogInRedirect from "@/component/LogInRedirect";
 import LogOut from "@/component/LogOut";
+import { useRouter } from "next/navigation";
 
 const Extra = () => {
+  const router = useRouter();
   const { data: session, status } = useSession();
   if (status === "loading") {
     return <div>Loadin....</div>;
   }
-  if (!session) {
-   return<LogInRedirect/>
+  // if (!session) {
+  //   return <LogInRedirect />;
+  // }
+  if (status === "unauthenticated") {
+    router.push("http://localhost:3000/api/auth/signin/github")
   }
   return (
     <div>
